5c00052206b9e69d368074c26365e098539a3e2b
acevedoleonardo <leonardo1191@gmail.com>
2025-04-29 06:55:13 -0500

    feat: :twisted_rightwards_arrows: Creacion de ramas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Prueba"); //Salida de Consola. 
+function clima(){ //Se crea una funcion llamada clima 
+    let xhr = new XMLHttpRequest();
+    let link= "http://api.weatherapi.com/v1/forecast.json?key=13cfdc7ae8984099b5350320252904&q=Floridablanca&lang=es&days=14"; //Obtener el pronostico del clima
+    xhr.open('GET',link,true);
+    xhr.onreadystatechange = function(){
+        if(this.status ==200){
+            let respuesta = JSON.parse(this.responseText);
+            let Temperatura=respuesta["current"]["temp_c"]
+            console.log(Temperatura); 
+            displayclimainicio(respuesta);
+        }
+    };
+    xhr.send();
+}
+clima();
